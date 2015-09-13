15b796d5daa3f93b7d269d9a5822e57de39a8ab8
skru <skruproxy@gmail.com>
2015-09-13 16:04:10 +0100

    multiple cats

--- js/app.js
+++ js/app.js
@@ -1,24 +1,53 @@
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('jess');
-  this.imgsrc = ko.observable('http://i.telegraph.co.uk/multimedia/archive/02830/cat_2830677b.jpg')
-  
-  this.nickList = ko.observableArray(
-      [
-      { name: "Bungle" },
-      { name: "George" },
-      { name: "Zippy" }
-  ])
+// DATA //
+var initialCats = [
+  {
+    clickCount: 0,
+    name: 'tabbs',
+    imgsrc: 'http://i.telegraph.co.uk/multimedia/archive/02830/cat_2830677b.jpg',
+    nicknames: ['a','b','c']
+  },
+  {
+    clickCount: 0,
+    name: 'shnoop',
+    imgsrc: 'http://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg',
+    nicknames: ['t','w','4']
+  },
+  {
+    clickCount: 0,
+    name: 'doops',
+    imgsrc: 'http://static3.businessinsider.com/image/5238c9c5ecad047f12b2751a/internet-famous-grumpy-cat-just-landed-an-endorsement-deal-with-friskies.jpg',
+    nicknames: ['r','x','a']
+  }
+]
+// Model //////////////////////////////////////////
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgsrc = ko.observable(data.imgsrc)
+  this.nickList = ko.observableArray(data.nicknames)
 };
 
 
 
-
+// ViewModel ///////////////////////////////////////
 var ViewModel = function() {
+  
   var self = this;
-  this.currentCat = ko.observable( new Cat() );
+  
+  this.catList = ko.observableArray([]);
+  
+  initialCats.forEach(function(catItem){
+    self.catList.push( new Cat(catItem) );
+  })
+  
+  this.currentCat = ko.observable( this.catList()[0]);
+  
   this.incrementCounter = function() {
     self.currentCat().clickCount(self.currentCat().clickCount() + 1)
+  };
+  
+  changeCat = function(e) {
+    self.currentCat(e);
   };
 };
 
